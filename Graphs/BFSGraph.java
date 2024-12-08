4521140f8aa2f1d45ab3c01993a387790e9c85c2
sireesha0904 <modisireesha09@gmail.com>
2024-12-08 20:18:48 +0530

    PR

--- Graphs/BFSGraph.java
+++ Graphs/BFSGraph.java
@@ -5,4 +5,12 @@ import java.util.*;
 class BFSGraph {
     private List<List<Integer>> adjeList;
     private int vertices;
+
+    BFSGraph(int vertices) {
+        this.vertices = vertices;
+        adjeList = new ArrayList<>();
+        for (int i = 0; i < vertices; i++) {
+            adjeList.add(new ArrayList<>());
+        }
+    }
 }
