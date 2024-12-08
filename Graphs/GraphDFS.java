6c089899301d73fc283bf0698c12d6b1e785c6b8
sireesha0904 <modisireesha09@gmail.com>
2024-12-08 20:57:46 +0530

    dfs

--- Graphs/GraphDFS.java
+++ Graphs/GraphDFS.java
@@ -1,5 +1,11 @@
 package Graphs;
 import java.util.*;
 public class GraphDFS {
-    
+    private List<List<Integer>> adjecList;
+    private int vertices;
+
+    GraphDFS(int vertices) {
+        this.vertices = vertices;
+        adjecList = new ArrayList<>();
+    }
 }
