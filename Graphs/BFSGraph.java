a03dffa7f6981c6facd8d26436dcfb30c1eb3c77
sireesha0904 <modisireesha09@gmail.com>
2024-12-08 20:49:38 +0530

    bfs

--- Graphs/BFSGraph.java
+++ Graphs/BFSGraph.java
@@ -19,6 +19,38 @@ class BFSGraph {
         adjeList.get(destination).add(source); // Add source to the destination`s list
     }
 
+    public void bfs(int startVertex) {
+        boolean[] visited = new boolean[vertices]; // To track the visited nodes
+        Queue<Integer> queue = new LinkedList<>(); // Queue of vertices to visit
+
+        visited[startVertex] = true; // mark this vertex as visited
+        queue.add(startVertex); // Add the starting vertex to the queue
+        System.out.println("Breadth-First Search: ");
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            System.out.print(current + " ");
+
+            for (int neighbor : adjeList.get(current)) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.add(neighbor);
+                }
+            }
+        }
+        System.out.println();
+     }
+    public static void main(String[] args) {
+        BFSGraph grap = new BFSGraph(6);
+         // Add edges to the graph
+    grap.addEdge(0, 1);
+    grap.addEdge(0, 2);
+    grap.addEdge(1, 3);
+    grap.addEdge(1, 4);
+    grap.addEdge(2, 5);
+
+    grap.bfs(0);
+    }
 }
 
 
