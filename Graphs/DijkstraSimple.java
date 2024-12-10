b60fc19c2bb102526686b995f9cf00a98c822c3e
sireesha0904 <modisireesha09@gmail.com>
2024-12-10 20:52:28 +0530

    pr

--- Graphs/DijkstraSimple.java
+++ Graphs/DijkstraSimple.java
@@ -2,19 +2,5 @@ package Graphs;
 import java.util.*;
 
 public class DijkstraSimple {
-    static class Node implements Comparable<Node> {
-        int vertices;
-        int weight;
-
-        Node(int vertices, int weight) {
-            this.vertices = vertices;
-            this.weight = weight;
-        }
-
-        @Override
-        public int compareTo(Node other) {
-            return this.weight - other.weight;
-        }
-    }
-
+    
 }
