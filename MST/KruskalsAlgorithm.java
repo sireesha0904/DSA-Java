799a65806ee9c425301b6641923c3e89c4a1f3f2
sireesha0904 <modisireesha09@gmail.com>
2024-12-13 10:27:16 +0530

    pr

--- MST/KruskalsAlgorithm.java
+++ MST/KruskalsAlgorithm.java
@@ -3,7 +3,29 @@ import java.util.*;
 
 public class KruskalsAlgorithm {
     // Represents an edge between two nodes with a weight
-   
+    static class Edge {
+        int src, dest, weight;
+
+        Edge(int src, int dest, int weight) {
+            this.src = src;
+            this.dest = dest;
+            this.weight = weight;
+        }
+    }
+
+    static class DisjointSet {
+        int[] parent, rank;
+
+        DisjointSet(int n) {
+            parent = new int[n];
+            rank = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                rank[i] = 0;
+
+            }
+        }
+    }
     public static void main(String[] args) {
         
     }
