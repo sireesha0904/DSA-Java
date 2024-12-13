0fe45625f894ece12778e3d0bf9d19f979abca33
sireesha0904 <modisireesha09@gmail.com>
2024-12-13 18:45:56 +0530

    str hashing

--- Hashing/HashCodeExampleStr.java
+++ Hashing/HashCodeExampleStr.java
@@ -1,3 +1,8 @@
+package Hashing;
 public class HashCodeExampleStr {
-    
+    public static void main(String[] args) {
+        String str = "siri";
+        int hashCode = str.hashCode();
+        System.out.println(hashCode);
+    }
 }
