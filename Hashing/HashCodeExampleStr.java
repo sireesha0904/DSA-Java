53ea10fddfb28de9e2ef46412e1004ef6cacb499
sireesha0904 <modisireesha09@gmail.com>
2024-12-13 21:05:09 +0530

    pr

--- Hashing/HashCodeExampleStr.java
+++ Hashing/HashCodeExampleStr.java
@@ -1,8 +1,8 @@
 package Hashing;
 public class HashCodeExampleStr {
     public static void main(String[] args) {
-        int str = 1509;
-        int hashCode = Integer.hashCode(str); // Correct way
-        System.out.println(hashCode); // Outputs 1509 (same as the value for integers)
+        String str = "siri";
+        int hashCode = str.hashCode();
+        System.out.println(hashCode);
     }
 }
