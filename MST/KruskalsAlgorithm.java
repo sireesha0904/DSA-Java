2505c88c89c9074ae80f000c8d58f54869a09b34
sireesha0904 <modisireesha09@gmail.com>
2024-12-13 09:44:35 +0530

    Prims

--- MST/KruskalsAlgorithm.java
+++ MST/KruskalsAlgorithm.java
@@ -1,6 +1,22 @@
 package MST;
+import java.util.*;
 
 public class KruskalsAlgorithm {
+    // Represents an edge between two nodes with a weight
+    static class Edge implements Comparable<Edge> {
+        int src, dest, weight;
+
+        Edge(int src, int dest, int weight) {
+            this.src = src;
+            this.dest = dest;
+            this.weight = weight;
+        }
+
+        public int compareTo(Edge other) {
+            return this.weight - other.weight;
+        }
+    
+    }
     public static void main(String[] args) {
         
     }
