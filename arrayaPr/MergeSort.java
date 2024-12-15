9baaab1c0347c967c536008f1d6539b102fe81c5
sireesha0904 <modisireesha09@gmail.com>
2024-12-15 19:24:50 +0530

    pr

--- arrayaPr/MergeSort.java
+++ arrayaPr/MergeSort.java
@@ -24,7 +24,7 @@ public class MergeSort {
         while (i < left.length && j < right.length) {
             if (left[i] <= right[j]) 
             {
-                array[k++] = left[k++];
+                array[k++] = left[i++];
             }
             else {
                 array[k++] = right[j++];
@@ -38,10 +38,11 @@ public class MergeSort {
         }
     }
     public static void main(String[] args) {
-        int[] array = { 38, 27, 43, 3, 9, 82, 10 };
-        System.out.println("Original Array:" + Arrays.toString(array));
-        System.out.println("Sorted Array:" + Arrays.toString(array));
-        mergeSort(array);
-        System.out.println("Sorted Array:" + Arrays.toString(array));
+       int[] array = {38, 27, 43, 3, 9, 82, 10};
+        System.out.println("Original Array: " + Arrays.toString(array));
+        
+        mergeSort(array); // Call mergeSort to sort the array
+        
+        System.out.println("Sorted Array: " + Arrays.toString(array));
     }
 }
