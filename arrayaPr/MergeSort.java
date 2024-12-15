c46fed6a6e543b7ef286f5a9f2f30b8e2c75d2bd
sireesha0904 <modisireesha09@gmail.com>
2024-12-15 18:27:17 +0530

    pr

--- arrayaPr/MergeSort.java
+++ arrayaPr/MergeSort.java
@@ -1,6 +1,11 @@
 package arrayaPr;
 
 public class MergeSort {
+    public static void mergeSort(int[] array) {
+        if (array.length < 2) {
+            return;  // base condition
+        }
+    }
     public static void main(String[] args) {
         
     }
