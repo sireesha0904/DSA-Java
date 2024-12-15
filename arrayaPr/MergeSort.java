38f500a0d9e22bdf3e323960f38d9cd0d121b3c2
sireesha0904 <modisireesha09@gmail.com>
2024-12-15 18:45:09 +0530

    pr

--- arrayaPr/MergeSort.java
+++ arrayaPr/MergeSort.java
@@ -1,12 +1,27 @@
 package arrayaPr;
+import java.util.Arrays;
 
 public class MergeSort {
     public static void mergeSort(int[] array) {
+
+        //STEP-1: Base condition (stop splitting if the array has 1 or 0 elements)
         if (array.length < 2) {
-            return;  // base condition
+            return; // base condition
         }
+        
+        // Step 2: Split the array into two halves
+        int mid = array.length / 2;  // Find the middle index
+        int[] left = Arrays.copyOfRange(array, 0, mid); // Copy the left half 
+        int[] right = Arrays.copyOfRange(array, mid, array.length); // Copy the right half
+
+        mergeSort(left);
+        mergeSort(right);
     }
     public static void main(String[] args) {
-        
+        int[] array = { 38, 27, 43, 3, 9, 82, 10 };
+        System.out.println("Original Array:" + Arrays.toString(array));
+        System.out.println("Sorted Array:" + Arrays.toString(array));
+        mergeSort(array);
+        System.out.println("Sorted Array:" + Arrays.toString(array));
     }
 }
