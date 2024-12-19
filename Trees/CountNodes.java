cafe498d76b68ac2944d4164fcc7dee9a231e55a
sireesha0904 <modisireesha09@gmail.com>
2024-12-19 20:32:49 +0530

    pr

--- Trees/CountNodes.java
+++ Trees/CountNodes.java
@@ -30,6 +30,8 @@ public class CountNodes {
         tree.root.right = new Node(3);
         tree.root.left.left = new Node(4);
         tree.root.left.right = new Node(5);
+        tree.root.right.left = new Node(6);
+        tree.root.right.right = new Node(7);
 
         int result = tree.count(tree.root);
         System.out.println("Number of nodes in the tree: " + result);
