c2a42707117cea3676444f5af30c7d6d721da0a0
sireesha0904 <modisireesha09@gmail.com>
2024-12-28 13:03:12 +0530

    pr

---
+++ BitManipulation/Swap.java
@@ -0,0 +1,16 @@
+package BitManipulation;
+
+public class Swap {
+    public static void swap(int a, int b) {
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        System.out.println("After swapping, a = " + a + " and b = " + b);
+    }
+
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 15;
+        swap(a, b);
+    }
+}
