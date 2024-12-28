2a2c6f2f3f1854b5ed8d8efa4398fafd8ae7c2fd
sireesha0904 <modisireesha09@gmail.com>
2024-12-28 12:56:46 +0530

    pr

--- BitManipulation/UpdateBit.java
+++ BitManipulation/UpdateBit.java
@@ -1,5 +1,26 @@
 package BitManipulation;
 
 public class UpdateBit {
-    
+    public static int setBit(int number, int position) {
+        return number | (1 << position);
+    }
+
+    public static int clearBit(int number, int position) {
+        return number & ~(1 << position);
+    }
+
+    public static int updateBit(int number, int position, int value) {
+        if (value == 0) {
+            return clearBit(number, position);
+        } else {
+            return setBit(number, position);
+        }
+    }
+    public static void main(String[] args) {
+        int number = 0b0101;
+        int position = 1;
+        int value = 1;
+        int result = updateBit(number, position, value);
+        System.out.println("The bit at position " + position + " in " + number + " is " + result);
+    }
 }
