91ae34ff34e3fd7eb4d1ea831e35a7fc1dc1962e
sireesha0904 <modisireesha09@gmail.com>
2024-12-28 10:30:34 +0530

    pr

--- BitManipulation/GetBit.java
+++ BitManipulation/GetBit.java
@@ -2,8 +2,13 @@ package BitManipulation;
 
 public class GetBit {
     
+    public static int getBit(int number, int position) {
+        return (number >> position) & 1;
+    }
     public static void main(String[] args) {
         int number = 0101;
         int position = 2;
+        int result = getBit(number, position);
+        System.out.println("The bit at position " + position + " in " + number + " is " + result);
     }
 }
