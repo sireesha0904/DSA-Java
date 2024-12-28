f0d3da402f2490c06f246d7e38bab8d47e281431
sireesha0904 <modisireesha09@gmail.com>
2024-12-28 11:48:56 +0530

    pr

--- BitManipulation/SetBit.java
+++ BitManipulation/SetBit.java
@@ -1,7 +1,14 @@
 package BitManipulation;
 
 public class SetBit {
+    public static int setBit(int number, int position) {
+        return (number | (1 << position));
+    }
     public static void main(String[] args) {
-        
+        int number = 0b0101;
+        int position = 1;
+        int result = setBit(number, position);
+        System.out.println("The bit at position " + position + " in " + number + " is " + result);
+        System.out.println("In Binary representation of " + result + " is " + Integer.toBinaryString(result) );
     }
 }
