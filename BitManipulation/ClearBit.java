d50c9c069cf4f7ad4d749f35d92902b74cb0588e
sireesha0904 <modisireesha09@gmail.com>
2025-01-03 21:12:25 +0530

    pr

--- BitManipulation/ClearBit.java
+++ BitManipulation/ClearBit.java
@@ -1,14 +1,16 @@
 package BitManipulation;
 
-public class ClearBitt {
+public class ClearBit {
 
     public static int clearBit(int number, int position) {
         return number & ~(1 << position);
     }
+
     public static void main(String[] args) {
-        int number = 0b0101;
-        int position = 2;
+        int number = 0b0101; // Binary representation of 5
+        int position = 2;    // Zero-based position to clear
         int result = clearBit(number, position);
+
         System.out.println("Number before clearing the bit: " + Integer.toBinaryString(number));
         System.out.println("Number after clearing the bit: " + Integer.toBinaryString(result));
     }
