a6d5e6cfac2650e9519da528745c60216779fd9b
sireesha0904 <modisireesha09@gmail.com>
2025-01-03 21:01:09 +0530

    pr

--- BitManipulation/clear.java
+++ BitManipulation/clear.java
@@ -1,5 +1,14 @@
 package BitManipulation;
 
 public class clear {
-    
+
+    public int clearBit(int number, int position) {
+        return number & ~(1 << position);
+    }
+    public static void main(String[] args) {
+        int number = 0b0101;
+        int position = 1;
+        int result = new clear().clearBit(number, position);
+        System.out.println("The bit at position " + position + " in " + number + " is " + result);
+    }
 }
