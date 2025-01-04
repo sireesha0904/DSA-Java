0607095c9381d3865f5e4139e694b4fc036a65b1
sireesha0904 <modisireesha09@gmail.com>
2025-01-04 07:47:49 +0530

    pr

--- BitManipulation/CheckPowerOfTwo.java
+++ BitManipulation/CheckPowerOfTwo.java
@@ -1,4 +1,14 @@
 package BitManipulation;
 public class CheckPowerOfTwo {
+    public static boolean isPowerOfTwo(int n) {
+        return n >0 && (n & (n-1)) == 0;
+    }
     
+    public static void main(String[] args) {
+        int n = 8;
+        System.out.println("Checking power of two: " + isPowerOfTwo(n));
+
+        n = 7;
+        System.out.println("Checking power of two: " + isPowerOfTwo(n));
+    }
 }
