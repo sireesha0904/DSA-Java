0751a7717fbae02e1ca9764047fe34e7781ae8f5
sireesha0904 <modisireesha09@gmail.com>
2025-01-06 19:21:55 +0530

    pr

--- BitManipulation/UpdateBit.java
+++ BitManipulation/UpdateBit.java
@@ -1,5 +1,5 @@
 package BitManipulation;
-//combinaion of set and clear bit
+//combinaion of set and clear bit is called Updatebit
 public class UpdateBit {
     public static int setBit(int number, int position) {
         return number | (1 << position);
