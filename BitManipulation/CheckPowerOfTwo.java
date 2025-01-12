bf405723692fa6b9345e834fe82469bc6ce8b0d3
Modi Sireesha <131472116+sireesha0904@users.noreply.github.com>
2025-01-12 18:54:01 +0530

    Update CheckPowerOfTwo.java

--- BitManipulation/CheckPowerOfTwo.java
+++ BitManipulation/CheckPowerOfTwo.java
@@ -1,1 +1,16 @@
+package BitManipulation;
+//Program to check the power of two
+public class CheckPowerOfTwo {
+    public static boolean isPowerOfTwo(int n) {
+        return n >0 && (n & (n-1)) == 0;
+    }
+    
+    public static void main(String[] args) {
+        int n = 8;
+        System.out.println("Checking power of "+n + " is " + isPowerOfTwo(n));
+
+        n = 7;
+        System.out.println("Checking power of "+n + " is " + isPowerOfTwo(n));
+    }
+}
 
