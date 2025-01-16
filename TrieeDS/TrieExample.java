f2d1ca3cfbdbbe4cbb8fc1c7b865bd92ed96d014
sireesha0904 <modisireesha09@gmail.com>
2025-01-16 08:08:38 +0530

    pr

--- TrieeDS/TrieExample.java
+++ TrieeDS/TrieExample.java
@@ -1,5 +1,20 @@
 package TrieeDS;
 
-public class TrieExample {
+class TrieNode {
+    TrieNode[] children = new TrieNode[26];
+    boolean isEndOfWord = false;
+}
+
+class Trie {
+    private TrieNode root;
     
+    public Trie() {
+        root = new TrieNode(); // Initialize the root node
+    }
+}
+
+public class TrieExample {
+    public static void main(String[] args) {
+        
+    }
 }
