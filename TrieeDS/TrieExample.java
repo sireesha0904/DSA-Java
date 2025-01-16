209140c63110fd93a257c353867c6d3f9748d9ea
sireesha0904 <modisireesha09@gmail.com>
2025-01-16 08:30:54 +0530

    pr

--- TrieeDS/TrieExample.java
+++ TrieeDS/TrieExample.java
@@ -7,14 +7,31 @@ class TrieNode {
 
 class Trie {
     private TrieNode root;
-    
+
     public Trie() {
         root = new TrieNode(); // Initialize the root node
+    }
+
+    // Insert a word into the trie
+    public void Insert(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+
+            if (node.children[index] == null) {
+                node.children[index] = new TrieNode();
+                node = node.children[index];
+
+            }
+
+        }
+        node.isEndOfWord = true;
     }
 }
 
 public class TrieExample {
     public static void main(String[] args) {
-        
+        Trie trie = new Trie();
+        trie.Insert("Sireesha");
     }
 }
