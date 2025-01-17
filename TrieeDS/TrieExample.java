fa8aefc03d8598c28e9702ea2f95132a6c06417b
sireesha0904 <modisireesha09@gmail.com>
2025-01-17 07:48:30 +0530

    Implementation of Triee DS

--- TrieeDS/TrieExample.java
+++ TrieeDS/TrieExample.java
@@ -1,4 +1,9 @@
 package TrieeDS;
+
+import java.util.TreeMap;
+
+import javax.swing.tree.TreeNode;
+
 // Program for insert and search 
 class TrieNode {
     TrieNode[] children = new TrieNode[26];
@@ -13,7 +18,17 @@ class Trie {
     }
 
     // Insert a word into the trie
- 
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char c : word.toLowerCase().toCharArray()) {
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new TrieNode(); //  Create new node if it does not exist
+            }
+            node = node.children[index];
+        }
+        node.isEndOfWord = true;
+    }
 
     // Search for a word in the trie
   
