aaf1feff2951803c79ce3ad9b635e6ceb37e5031
sireesha0904 <modisireesha09@gmail.com>
2025-01-24 22:51:09 +0530

    analysis of growth rate

--- AnalysisOfAlgorithms/Linear.java
+++ AnalysisOfAlgorithms/Linear.java
@@ -1,5 +1,18 @@
 package AnalysisOfAlgorithms;
 
 public class Linear {
-    
+    static void linearSearch(String[] arr, String target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                System.out.println("Branch is : " + i);
+                return;
+            }
+        }
+        System.out.println("Branch is not found");
+    }
+    public static void main(String[] args) {
+        String[] arr = { "ECE", "EEE", "CSE", "IT", "Civil"};
+        String target = "CSE";
+        linearSearch(arr, target);
+    }
 }
