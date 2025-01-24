9a6c64659f6b93f34fc1e242d73a03d6bfec4874
sireesha0904 <modisireesha09@gmail.com>
2025-01-24 21:44:17 +0530

    analysis of an algorithms

--- AnalysisOfAlgorithms/ConstTime.java
+++ AnalysisOfAlgorithms/ConstTime.java
@@ -1,5 +1,16 @@
 package AnalysisOfAlgorithms;
 
 public class ConstTime {
+    public static void main(String[] args) {
+        CheckEvenOdd(5);
+    }
     
+    static void CheckEvenOdd(int n) {
+        if (n % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+
 }
